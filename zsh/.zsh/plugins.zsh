b89264598d8317d6c1e82eda755a81185b2d64cf
Alejandro Flores <dev.alejandro.flores@gmail.com>
2026-06-07 22:01:03 -0700

    fix: the same XD

--- zsh/.zsh/plugins.zsh
+++ zsh/.zsh/plugins.zsh
@@ -1,4 +1,4 @@
 
-source /usr/share/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /usr/share/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
