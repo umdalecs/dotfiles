373b66418b75e1ffe4b3d2105c4dc2aa4e7fd131
Alejandro Flores <dev.alejandro.flores@gmail.com>
2026-08-30 19:45:29 -0700

    removed steam from config for laptop

--- configuration.nix
+++ configuration.nix
@@ -40,7 +40,9 @@
   # services.printing.enable = true;
 
   services.pulseaudio.enable = false;
+
   security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -59,16 +61,14 @@
       "wheel" 
       "docker"
     ];
-    packages = with pkgs; [
-    ];
   };
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
+  #programs.steam = {
+  #  enable = true;
+  #  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  #  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  #};
 
   environment.systemPackages = with pkgs; [
     gnomeExtensions.dash-to-dock
@@ -123,7 +123,6 @@
       };
     };
   };
-
 
   virtualisation.docker.enable = true;
 
